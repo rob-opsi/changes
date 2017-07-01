8ca79728b953763189a5bcc3ec55bdaead5f91af
David Cramer <dcramer@gmail.com>
2017-07-01 01:17:26 -0700

    fix import code

--- changes/utils/imports.py
+++ changes/utils/imports.py
@@ -1,5 +1,7 @@
+from __future__ import absolute_import
+
 import pkgutil
-import sys
+import six
 
 
 class ModuleProxyCache(dict):
@@ -17,6 +19,7 @@ class ModuleProxyCache(dict):
 
         return handler
 
+
 _cache = ModuleProxyCache()
 
 
@@ -31,9 +34,16 @@ def import_string(path):
 
 
 def import_submodules(context, root_module, path):
-    for loader, module_name, is_pkg in pkgutil.walk_packages(path):
-        module = loader.find_module(module_name).load_module(module_name)
-        for k, v in vars(module).iteritems():
+    """
+    Import all submodules and register them in the ``context`` namespace.
+
+    >>> import_submodules(locals(), __name__, __path__)
+    """
+    for loader, module_name, is_pkg in pkgutil.walk_packages(path, root_module + '.'):
+        # this causes a Runtime error with model conflicts
+        # module = loader.find_module(module_name).load_module(module_name)
+        module = __import__(module_name, globals(), locals(), ['__name__'])
+        for k, v in six.iteritems(vars(module)):
             if not k.startswith('_'):
                 context[k] = v
-        sys.modules['{0}.{1}'.format(root_module, module_name)] = module
+        context[module_name] = module
